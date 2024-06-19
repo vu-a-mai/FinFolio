f01c9a266e34746f6c262adcf8e67bc4b0c3d6f3
Vu Mai <vuanhmai.io@gmail.com>
2024-06-18 20:15:05 -0700

    - Updated Hero Title and Content

--- Frontend-Vite-React-TypeScript/frontend/src/Components/Hero/Hero.tsx
+++ Frontend-Vite-React-TypeScript/frontend/src/Components/Hero/Hero.tsx
@@ -10,11 +10,11 @@ const Hero = (props: Props) => {
       <div className="container flex flex-col-reverse mx-auto p-8 lg:flex-row">
         <div className="flex flex-col space-y-10 mb-44 m-10 lg:m-10 xl:m-20 lg:mt:16 lg:w-1/2 xl:mb-52">
           <h1 className="text-5xl font-bold text-center lg:text-6xl lg:max-w-md lg:text-left">
-            Financial data with no news.
+            Ad-Free Financial Data
           </h1>
           <p className="text-2xl text-center text-gray-400 lg:max-w-md lg:text-left">
-            Search relevant financial documents without fear mongering and fake
-            news.
+            Search for relevant financial documents without ads or news
+            cluttering your search.
           </p>
           <div className="mx-auto lg:mx-0">
             <Link
